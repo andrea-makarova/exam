5757f464ba5ac92e84167fd4d8c3c895a3e2733c
andrea <andreamakarova99@gmail.com>
2019-01-06 19:04:39 +0100

    first

---
+++ exam.js
@@ -0,0 +1,16 @@
+function btn3() {
+    window.location = "https://www.linkedin.com/in/andrea-makarov%C3%A1-92b67216b/";
+};
+
+function show() {
+    section.style.display = "block";
+};
+
+function sho() {
+    sect.style.display = "block";
+};
+
+
+function clos() {
+    window.location = "profile.html";
+};
