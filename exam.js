bfd648932580c47177b64468c343a05b928f8db1
andrea <andreamakarova99@gmail.com>
2019-01-07 21:31:26 +0100

    third

--- exam.js
+++ exam.js
@@ -1,6 +1,103 @@
-function btn3() {
-    window.location = "https://www.linkedin.com/in/andrea-makarov%C3%A1-92b67216b/";
+function linkedin() {
+    window.location = "https://www.linkedin.com/in/andrea-makarov%C3%A1-x92b67216b/";
 };
+
+function btn1() {
+    description1.style.display = "block";
+    h1.style.display = "none";
+    onepic.style.opacity = "1";
+};
+
+function btn2() {
+    description2.style.display = "block";
+    h2.style.display = "none";
+    twopic.style.opacity = "1";
+};
+
+function btn3() {
+    description3.style.display = "block";
+    h3.style.display = "none";
+    threepic.style.opacity = "1";
+};
+
+function btn4() {
+    description4.style.display = "block";
+    h4.style.display = "none";
+    fourpic.style.opacity = "1";
+};
+
+function btn5() {
+    description5.style.display = "block";
+    h5.style.display = "none";
+    fivepic.style.opacity = "1";
+};
+
+function btn6() {
+    description6.style.display = "block";
+    h6.style.display = "none";
+    sixpic.style.opacity = "1";
+};
+
+function btn7() {
+    description7.style.display = "block";
+    h7.style.display = "none";
+    sevenpic.style.opacity = "1";
+};
+
+function btn8() {
+    description8.style.display = "block";
+    h8.style.display = "none";
+    eightpic.style.opacity = "1";
+};
+
+function btn9() {
+    description9.style.display = "block";
+    h9.style.display = "none";
+    ninepic.style.opacity = "1";
+};
+
+function btn10() {
+    description10.style.display = "block";
+    h10.style.display = "none";
+    tenpic.style.opacity = "1";
+};
+
+function btn11() {
+    description11.style.display = "block";
+    h11.style.display = "none";
+    elevenpic.style.opacity = "1";
+};
+
+function btn12() {
+    description12.style.display = "block";
+    h12.style.display = "none";
+    twelvepic.style.opacity = "1";
+};
+
+function btn13() {
+    description13.style.display = "block";
+    h13.style.display = "none";
+    thirteenpic.style.opacity = "1";
+};
+
+function btn14() {
+    description14.style.display = "block";
+    h14.style.display = "none";
+    fourteenpic.style.opacity = "1";
+};
+
+function btn15() {
+    description15.style.display = "block";
+    h15.style.display = "none";
+    fifteenpic.style.opacity = "1";
+};
+
+function btn16() {
+    description16.style.display = "block";
+    h16.style.display = "none";
+    sixteenpic.style.opacity = "1";
+};
+
 
 function show() {
     section.style.display = "block";
@@ -10,7 +107,7 @@ function sho() {
     sect.style.display = "block";
 };
 
-
 function clos() {
     window.location = "profile.html";
 };
+
