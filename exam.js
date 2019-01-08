150b706fa0d24bb6edb957ba68a32ba9df11eb37
andrea <andreamakarova99@gmail.com>
2019-01-08 20:47:38 +0100

    final

--- exam.js
+++ exam.js
@@ -98,6 +98,101 @@ function btn16() {
     sixteenpic.style.opacity = "1";
 };
 
+function cls1() {
+    description1.style.display = "none";
+    h1.style.display = "block";
+    onepic.style.opacity = "0.1";
+};
+
+function cls2() {
+    description2.style.display = "none";
+    h2.style.display = "block";
+    twopic.style.opacity = "0.1";
+};
+
+function cls3() {
+    description3.style.display = "none";
+    h3.style.display = "block";
+    threepic.style.opacity = "0.1";
+};
+
+function cls4() {
+    description4.style.display = "none";
+    h4.style.display = "block";
+    fourpic.style.opacity = "0.1";
+};
+
+function cls5() {
+    description5.style.display = "none";
+    h5.style.display = "block";
+    fivepic.style.opacity = "0.1";
+};
+
+function cls6() {
+    description6.style.display = "none";
+    h6.style.display = "block";
+    sixpic.style.opacity = "0.1";
+};
+
+function cls7() {
+    description7.style.display = "none";
+    h7.style.display = "block";
+    sevenpic.style.opacity = "0.1";
+};
+
+function cls8() {
+    description8.style.display = "none";
+    h8.style.display = "block";
+    eightpic.style.opacity = "0.1";
+};
+
+function cls9() {
+    description9.style.display = "none";
+    h9.style.display = "block";
+    ninepic.style.opacity = "0.1";
+};
+
+function cls10() {
+    description10.style.display = "none";
+    h10.style.display = "block";
+    tenpic.style.opacity = "0.1";
+};
+
+function cls11() {
+    description11.style.display = "none";
+    h11.style.display = "block";
+    elevenpic.style.opacity = "0.1";
+};
+
+function cls12() {
+    description12.style.display = "none";
+    h12.style.display = "block";
+    twelvepic.style.opacity = "0.1";
+};
+
+function cls13() {
+    description13.style.display = "none";
+    h13.style.display = "block";
+    thirteenpic.style.opacity = "0.1";
+};
+
+function cls14() {
+    description14.style.display = "none";
+    h14.style.display = "block";
+    fourteenpic.style.opacity = "0.1";
+};
+
+function cls15() {
+    description15.style.display = "none";
+    h15.style.display = "block";
+    fifteenpic.style.opacity = "0.1";
+};
+
+function cls16() {
+    description16.style.display = "none";
+    h16.style.display = "block";
+    sixteenpic.style.opacity = "0.1";
+};
 
 function show() {
     section.style.display = "block";
